b83371401fd93d278d8c3df9507790446d6bc0f7
DoHaiBac <bac.do@evizi.com>
2021-06-08 14:21:14 +0700

    update index file

--- index.js
+++ index.js
@@ -29,10 +29,7 @@ app.get("/api/course/:id", (req, res) => {
 app.post("/api/courses", (req, res) => {
 	const { error } = validateCourse(req.body);
 
-	if (error) {
-		res.status(400).send(error.details[0].message);
-		return;
-	}
+	if (error) return res.status(400).send(error.details[0].message);
 
 	const course = {
 		"id" : courses.length + 1, 
@@ -45,16 +42,11 @@ app.post("/api/courses", (req, res) => {
 
 app.put("/api/course/:id", (req, res) => {
 	const course = getCourse(req);
-	if (!course) {
-		res.status(404).send("The Course ID was not found");
-		return;
-	}
+	if (!course) return res.status(404).send("The Course ID was not found");
 
 	const { error } = validateCourse(req.body);
 
-	if (error) {
-		res.status(400).send(error.details[0].message);
-	}
+	if (error) return res.status(400).send(error.details[0].message);
 
 	course.name = req.body.name;
 	res.send(course);
